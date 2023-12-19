c8601b6f7622e17fad48cc1e84750b69853c58b3
Ryacuda <adam.germain1@gmail.com>
2023-12-19 14:48:53 +0100

    webcam + marker detection

--- Assets/Scripts/WebcamMarkerDetector.cs
+++ Assets/Scripts/WebcamMarkerDetector.cs
@@ -1,5 +1,6 @@
 using Emgu.CV;
 using Emgu.CV.Aruco;
+using Emgu.CV.Structure;
 using Emgu.CV.Util;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,30 +8,68 @@ using UnityEngine;
 
 public class WebcamMarkerDetector : MonoBehaviour
 {
+	[SerializeField] private List<int> markers;
+	[SerializeField] private double threshold_maxvalue;
+
+
+	private VideoCapture webcam;
+	private Mat webcame_frame;
+
 	// Start is called before the first frame update
 	void Start()
 	{
+		webcam = new VideoCapture(0, VideoCapture.API.DShow);
+		webcame_frame = new Mat();
+
+		if(webcam != null)
+		{
+			webcam.ImageGrabbed += new System.EventHandler(HandleWebcamQueryFrame);
+			webcam.Start();
+		}
 		
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+		if (webcam != null && webcam.IsOpened)
+		{
+			webcam.Grab();
+		}
+	}
+
+	private void HandleWebcamQueryFrame(object sender, System.EventArgs e)
+	{
+		if (webcam != null && webcam.IsOpened)
+		{
+			// retrieve frame
+			webcam.Retrieve(webcame_frame);
+
+			// detect marker
+			DetectMarkers();
+		}
 	}
 
 	private void DetectMarkers()
 	{
-        Emgu.CV.Mat img = new Mat("Assets\\Images\\35.png");
-
         VectorOfVectorOfPointF corners = new VectorOfVectorOfPointF();
         VectorOfInt marker_ids = new VectorOfInt();
         Dictionary dict = new Dictionary(Dictionary.PredefinedDictionaryName.Dict4X4_1000);
-        ArucoInvoke.DetectMarkers(img, dict, corners, marker_ids, DetectorParameters.GetDefault());
+
+        ArucoInvoke.DetectMarkers(webcame_frame, dict, corners, marker_ids, DetectorParameters.GetDefault());
 
         for (int i = 0; i < marker_ids.Size; i++)
         {
             Debug.Log(marker_ids[i]);
         }
     }
+
+	void OnDestroy()
+	{
+		if (webcam != null)
+		{
+			webcam.Stop();
+			webcam.Dispose();
+		}
+	}
 }
